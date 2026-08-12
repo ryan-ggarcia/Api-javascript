fba0444e4bba5842fe60c0f9d522ab7af2b79901
Suporte <168090412+ryan-ggarcia@users.noreply.github.com>
2026-08-12 14:07:38 -0300

    feat: explicações

--- controller/tasks_controller.js
+++ controller/tasks_controller.js
@@ -11,25 +11,30 @@ let taks_array = [{
 export default class TasksController{
 
     list(req,res){
+        // Devolve o array com status 200
         return res.status(200).json(taks_array)
     }
 
     insert(req,res){
+        // Pega o nome do corpo da requisição
         const {name} = req.body
         if (name){
+            // Guarda em uma variavel a nova tarefa
             let new_task = {id: Date.now(),name:name}
-            taks_array.push(new_task)
-            res.status(201).json(taks_array)
+            taks_array.push(new_task) // Adiciona a nova tarefa no array
+            res.status(201).json(taks_array) // Resposta de sucesso
         }else{
-            res.status(400).json("Params not defined!")
+            res.status(400).json("Params not defined!") // Resposta de erro
         }
     }
 
     update(req,res){
         const {id,name} = req.body
         if(id != null && id != undefined){
+            // Busca o id dentra variavel task_array
             let find_id = taks_array.find((task) => task.id == id)
             if (find_id != undefined){
+                // Substitui o nome do array pelo novo nome digitado
                 find_id.name = (name)
                 res.status(201).json(taks_array)
             }else{
@@ -43,7 +48,9 @@ export default class TasksController{
     delete(req,res){
         const {id} = req.body
         if ( id != null && id != undefined){
+            // Filtra apenas aquelas tarefas que são diferente do id digitado
             taks_array = taks_array.filter((task) => task.id != id)
+            // Busca a tarefa excluida para confirmar a sua exclusão
             if ( taks_array.find((task)=> task.id == id) == undefined )
                 res.status(201).json(taks_array)
             else
